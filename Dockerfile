23afa32f781c1287296934b8adf13830d8201bb7
Mehdi Abaakouk <sileht@sileht.net>
2022-03-23 20:41:21 +0100

    feat(docker): add docker setup
    This is a first step for automatically build docker images

---
+++ Dockerfile
@@ -0,0 +1,109 @@
+ARG ENGINE_PATH=mergify-engine
+ARG ENGINE_SIGNAL_PATH=mergify-engine-signals
+ARG PYTHON_VERSION=3
+
+### BASE ###
+FROM python:${PYTHON_VERSION}-slim as python-base
+RUN useradd -m mergify
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt update -y && apt upgrade -y && apt install -y git && apt autoremove --purge -y
+
+### BUILDER JS ###
+FROM node:16-buster-slim as js-builder
+ARG ENGINE_PATH
+# Real install that can't be cached
+ADD ${ENGINE_PATH}/installer /installer
+WORKDIR /installer
+RUN npm ci
+RUN npm run build
+RUN rm -rf node_modules
+
+### BUILDER PYTHON ###
+FROM python-base as python-builder
+ARG ENGINE_PATH
+ARG ENGINE_SIGNAL_PATH
+
+# Real install that can't be cached
+RUN apt install -y gcc
+
+RUN python3 -m venv /venv
+ENV VIRTUAL_ENV=/venv
+ENV PATH="/venv/bin:${PATH}"
+
+# First cache build requirements
+RUN python3 -m pip install wheel
+ADD ${ENGINE_PATH}/requirements.txt /
+# nosemgrep: generic.ci.security.use-frozen-lockfile.use-frozen-lockfile-pip
+RUN python3 -m pip install --no-cache-dir -r /requirements.txt
+
+ADD ${ENGINE_PATH} /app
+ADD ${ENGINE_SIGNAL_PATH} /signals
+
+# nosemgrep: generic.ci.security.use-frozen-lockfile.use-frozen-lockfile-pip
+RUN python3 -m pip install --no-cache-dir -c /requirements.txt -e /app
+# nosemgrep: generic.ci.security.use-frozen-lockfile.use-frozen-lockfile-pip
+RUN python3 -m pip install --no-cache-dir -c /requirements.txt -e /signals
+
+### BASE RUNNER ###
+FROM python-base as runner-base
+ARG PYTHON_VERSION
+ARG DD_AGENT_VERSION=7.34.0-1
+LABEL mergify-engine.version="$MERGIFYENGINE_VERSION"
+LABEL datadog-agent.version="$DD_AGENT_VERSION"
+LABEL python.version="$PYTHON_VERSION"
+
+# Add Datadog repository, signing keys and packages
+RUN apt-get update \
+ && apt-get install -y gnupg apt-transport-https gpg-agent curl ca-certificates
+ENV DATADOG_APT_KEYRING="/usr/share/keyrings/datadog-archive-keyring.gpg"
+ENV DATADOG_APT_KEYS_URL="https://keys.datadoghq.com"
+RUN sh -c "echo 'deb [signed-by=${DATADOG_APT_KEYRING}] https://apt.datadoghq.com/ stable 7' > /etc/apt/sources.list.d/datadog.list"
+RUN touch ${DATADOG_APT_KEYRING}
+RUN curl -o /tmp/DATADOG_APT_KEY_CURRENT.public "${DATADOG_APT_KEYS_URL}/DATADOG_APT_KEY_CURRENT.public" && \
+    gpg --ignore-time-conflict --no-default-keyring --keyring ${DATADOG_APT_KEYRING} --import /tmp/DATADOG_APT_KEY_CURRENT.public
+RUN curl -o /tmp/DATADOG_APT_KEY_F14F620E.public "${DATADOG_APT_KEYS_URL}/DATADOG_APT_KEY_F14F620E.public" && \
+    gpg --ignore-time-conflict --no-default-keyring --keyring ${DATADOG_APT_KEYRING} --import /tmp/DATADOG_APT_KEY_F14F620E.public
+RUN curl -o /tmp/DATADOG_APT_KEY_382E94DE.public "${DATADOG_APT_KEYS_URL}/DATADOG_APT_KEY_382E94DE.public" && \
+    gpg --ignore-time-conflict --no-default-keyring --keyring ${DATADOG_APT_KEYRING} --import /tmp/DATADOG_APT_KEY_382E94DE.public
+RUN apt-get update && apt-get -y --force-yes install --reinstall datadog-agent=1:${DD_AGENT_VERSION}
+
+
+RUN apt clean -y && rm -rf /var/lib/apt/lists/*
+COPY --from=python-builder /app /app
+COPY --from=python-builder /signals /signals
+COPY --from=python-builder /venv /venv
+ADD datadog-wrapper.sh /
+WORKDIR /app
+ENV VIRTUAL_ENV=/venv
+ENV PYTHONUNBUFFERED=1
+ENV PATH="/venv/bin:${PATH}"
+ENV MERGIFYENGINE_VERSION=$MERGIFYENGINE_VERSION
+USER mergify
+
+### WEB ###
+FROM runner-base as saas-web
+ENV PORT=8002
+EXPOSE $PORT
+USER mergify
+ENTRYPOINT ["/datadog-wrapper.sh", "gunicorn", "--worker-class=uvicorn.workers.UvicornH11Worker", "--statsd-host=localhost:8125", "--log-level=warning", "mergify_engine.web.asgi"]
+
+### WORKER-SHARED ###
+FROM runner-base as saas-worker-shared
+USER mergify
+ENTRYPOINT ["/datadog-wrapper.sh", "mergify-engine-worker", "--enabled-services=shared-stream"]
+
+### WORKER-DEDICATED ###
+FROM runner-base as saas-worker-dedicated
+ENTRYPOINT ["/datadog-wrapper.sh", "mergify-engine-worker", "--enabled-services=dedicated-stream,stream-monitoring,delayed-refresh"]
+
+### ON PREMISE ###
+FROM runner-base as onpremise
+ARG MERGIFYENGINE_VERSION=dev
+USER root
+COPY --from=js-builder /installer/build /app/installer/build
+ADD onpremise/Procfile /app/
+ADD onpremise/entrypoint.sh /
+ENV DD_DOGSTATSD_DISABLE=1
+ENV DD_TRACE_ENABLED=0
+USER mergify
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ datadog-wrapper.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+export DYNOHOST="$(hostname)"
+export DYNOTYPE=${DYNO%%.*}
+
+if [ -z "$DYNO" -o "$DYNOHOST" == "run" ]; then
+    exec "$@"
+fi
+
+export DD_CONF_DIR="/etc/datadog-agent"
+
+### Configure all datadog.yaml settings via env
+export DD_TAGS="dyno:$DYNO dynotype:$DYNOTYPE appname:$HEROKU_APP_NAME"
+# We want always to have the Dyno ID as a host alias to improve correlation
+export DD_HOST_ALIASES="$DYNOHOST"
+# Set the hostname to dyno name and ensure rfc1123 compliance.
+HAN="$(echo "$HEROKU_APP_NAME" | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g')"
+D="$(echo "$DYNO" | sed -e 's/[^a-zA-Z0-9.-]/-/g' -e 's/^-//g')"
+export DD_HOSTNAME="$HAN.$D"
+export DD_PROCESS_CONFIG_ENABLED=true
+export DD_LOGS_ENABLED=true
+export DD_LOGS_CONFIG_FRAME_SIZE=30000
+
+# Copy the empty config file
+cp -f "${DD_CONF_DIR}/datadog.yaml.example" "$DD_CONF_DIR/datadog.yaml"
+
+case $DYNOTYPE in
+    web)
+        export DD_EXTRA_TAGS="service:engine-web"
+
+        cat > "$DD_CONF_DIR/conf.d/process.d/conf.yaml" <<EOF
+init_config:
+
+instances:
+  - name: gunicorn-worker
+    search_string: ['^gunicorn: worker']
+    exact_match: false
+  - name: gunicorn-master
+    search_string: ['^gunicorn: master']
+    exact_match: false
+EOF
+
+        mkdir -p "$DD_CONF_DIR/conf.d/engine-web.d"
+        cat > "$DD_CONF_DIR/conf.d/engine-web.d/conf.yaml" <<EOF
+init_config:
+
+instances:
+
+logs:
+  - type: udp
+    port: 10518
+    source: python
+    service: engine-web
+    sourcecategory: sourcecode
+EOF
+        ;;
+
+    worker-*)
+        export DD_EXTRA_TAGS="service:engine-worker"
+
+        cat > "$DD_CONF_DIR/conf.d/process.d/conf.yaml" <<EOF
+init_config:
+
+instances:
+  - name: mergify-engine-worker
+    search_string: ['bin/mergify-engine-worker']
+    exact_match: false
+EOF
+
+        mkdir -p "$DD_CONF_DIR/conf.d/engine-worker.d"
+        cat > "$DD_CONF_DIR/conf.d/engine-worker.d/conf.yaml" <<EOF
+init_config:
+
+instances:
+
+logs:
+  - type: udp
+    port: 10518
+    source: python
+    service: engine-worker
+    sourcecategory: sourcecode
+EOF
+        ;;
+
+esac
+
+cat > "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml" <<EOF
+init_config:
+
+instances:
+  - host: <CACHE HOST>
+    port: <CACHE PORT>
+    password: <CACHE PASSWORD>
+    db: <CACHE DB>
+    ssl: <CACHE SSL>
+    ssl_cert_reqs: 0
+    slowlog-max-len: 0
+    tags:
+      - role:cache
+
+  - host: <STREAM HOST>
+    port: <STREAM PORT>
+    password: <STREAM PASSWORD>
+    db: <STREAM DB>
+    ssl: <STREAM SSL>
+    ssl_cert_reqs: 0
+    slowlog-max-len: 0
+    keys:
+      - streams
+      - attempts
+    tags:
+      - role:streams
+EOF
+
+
+REDIS_REGEX='^redis(s?)://([^:]*):([^@]+)@([^:]+):([^/?]+)\?db=([^&]*)'
+
+if [ -n "$MERGIFYENGINE_STORAGE_URL" ]; then
+    if [[ $MERGIFYENGINE_STORAGE_URL =~ $REDIS_REGEX ]]; then
+        [ "${BASH_REMATCH[1]}" ] && REDIS_SSL="true" || REDIS_SSL="false"
+        sed -i "s/<CACHE SSL>/$REDIS_SSL/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CACHE HOST>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CACHE PASSWORD>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CACHE PORT>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CACHE DB>/${BASH_REMATCH[6]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+    fi
+fi
+
+if [ -n "$MERGIFYENGINE_STREAM_URL" ]; then
+    if [[ $MERGIFYENGINE_STREAM_URL =~ $REDIS_REGEX ]]; then
+        [ "${BASH_REMATCH[1]}" ] && REDIS_SSL="true" || REDIS_SSL="false"
+        sed -i "s/<STREAM SSL>/$REDIS_SSL/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<STREAM HOST>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<STREAM PASSWORD>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<STREAM PORT>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<STREAM DB>/${BASH_REMATCH[6]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+    fi
+fi
+
+
+unset DD_CONF_DIR
+datadog-agent run &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml &
+
+exec /venv/bin/ddtrace-run "$@"
