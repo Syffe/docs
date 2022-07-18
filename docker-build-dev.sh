50d7bf94ac6f285c5646f6b740738cbb76dd4e79
Mehdi ABAAKOUK <sileht@sileht.net>
2022-07-18 09:45:21 +0200

    chore: sync docker-build-dev.sh with dashboard (#280)
    The dashboard CLI interface is simplier to use, do the same here.

--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -1,11 +1,20 @@
 #/bin/bash
 
-# NOTE(sileht): build date is always the same to leverage/test the layer caching mechanism
+
+TO_BUILD="${@:-saas-web saas-worker-shared saas-worker-dedicated onpremise}"
+
+# NOTE: build date is always the same to leverage/test the layer caching mechanism
 # if devs want to rebuild they just need to use --no-cache
-exec docker buildx build \
-    --platform linux/amd64 \
-    --build-arg PYTHON_VERSION="$(cut -d- -f2 runtime.txt)" \
-    --build-arg MERGIFYENGINE_SHA="$(git log -1 --format='%H')" \
-    --build-arg MERGIFYENGINE_VERSION=dev \
-    --build-arg BUILD_DATE="never" \
-    "$@"
+for img in ${TO_BUILD[@]}; do
+    # NOTE(sileht): build date is always the same to leverage/test the layer caching mechanism
+    # if devs want to rebuild they just need to use --no-cache
+    docker buildx build \
+        --platform linux/amd64 \
+        --build-arg PYTHON_VERSION="$(cut -d- -f2 runtime.txt)" \
+        --build-arg MERGIFYENGINE_SHA="$(git log -1 --format='%H')" \
+        --build-arg MERGIFYENGINE_VERSION=dev \
+        --build-arg BUILD_DATE="never" \
+	    --target "$img" \
+	    --tag engine-"$img" \
+	    .
+done
