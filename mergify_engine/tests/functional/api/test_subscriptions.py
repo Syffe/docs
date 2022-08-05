a362c5d45351c532f2defccfb212872e4827ad6d
Greesb <guillaumerisbourg@gmail.com>
2022-08-05 09:01:11 +0200

    test: Add tests for subscription features checks on API (#468)
    Depends-On: #464

---
+++ mergify_engine/tests/functional/api/test_subscriptions.py
@@ -0,0 +1,119 @@
+import yaml
+
+from mergify_engine import config
+from mergify_engine.tests.functional import base
+
+
+class TestSubscriptionsApi(base.FunctionalTestBase):
+    async def test_queue_freeze_subscription(self):
+        rules = {
+            "queue_rules": [
+                {
+                    "name": "urgent",
+                    "conditions": [
+                        "status-success=continuous-integration/fast-ci",
+                    ],
+                    "batch_max_wait_time": "15 s",
+                    "speculative_checks": 1,
+                    "batch_size": 3,
+                    "queue_branch_prefix": "urgent-",
+                },
+                {
+                    "name": "default",
+                    "conditions": [
+                        "status-success=continuous-integration/slow-ci",
+                    ],
+                    "speculative_checks": 2,
+                    "batch_size": 2,
+                    "batch_max_wait_time": "0 s",
+                },
+                {
+                    "name": "low-priority",
+                    "conditions": [
+                        "status-success=continuous-integration/slow-ci",
+                    ],
+                    "allow_inplace_checks": False,
+                    "checks_timeout": "10 m",
+                    "draft_bot_account": "mergify-test4",
+                },
+            ],
+            "pull_request_rules": [
+                {
+                    "name": "Merge priority high",
+                    "conditions": [
+                        f"base={self.main_branch_name}",
+                        "label=queue-urgent",
+                    ],
+                    "actions": {"queue": {"name": "urgent"}},
+                },
+                {
+                    "name": "Merge default",
+                    "conditions": [
+                        f"base={self.main_branch_name}",
+                        "label=queue",
+                    ],
+                    "actions": {"queue": {"name": "default"}},
+                },
+                {
+                    "name": "Merge low",
+                    "conditions": [
+                        f"base={self.main_branch_name}",
+                        "label=queue-low",
+                    ],
+                    "actions": {"queue": {"name": "low-priority"}},
+                },
+            ],
+        }
+
+        await self.setup_repo(yaml.dump(rules))
+
+        r = await self.app.get(
+            f"/v1/repos/{config.TESTING_ORGANIZATION_NAME}/{self.RECORD_CONFIG['repository_name']}/queue/default/freeze",
+            headers={
+                "Authorization": f"bearer {self.api_key_admin}",
+                "Content-type": "application/json",
+            },
+        )
+        assert r.status_code == 402
+
+    async def test_eventlogs_subscription(self):
+        rules = {
+            "pull_request_rules": [
+                {
+                    "name": "hello",
+                    "conditions": [f"base={self.main_branch_name}", "label=auto-merge"],
+                    "actions": {
+                        "comment": {
+                            "message": "Hello!",
+                        },
+                    },
+                },
+                {
+                    "name": "mergeit",
+                    "conditions": [f"base={self.main_branch_name}"],
+                    "actions": {
+                        "assign": {"users": ["mergify-test1"]},
+                        "label": {
+                            "add": ["need-review"],
+                            "remove": ["auto-merge"],
+                        },
+                        "merge": {},
+                    },
+                },
+            ]
+        }
+
+        await self.setup_repo(yaml.dump(rules))
+
+        p1 = await self.create_pr()
+        await self.add_label(p1["number"], "auto-merge")
+        await self.run_engine()
+
+        r = await self.app.get(
+            f"/v1/repos/{config.TESTING_ORGANIZATION_NAME}/{self.RECORD_CONFIG['repository_name']}/pulls/{p1['number']}/events",
+            headers={
+                "Authorization": f"bearer {self.api_key_admin}",
+                "Content-type": "application/json",
+            },
+        )
+        assert r.status_code == 402
