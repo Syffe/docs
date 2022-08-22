8650c15805b21e8ddd877e18d88473b09ae7c0a9
Mehdi ABAAKOUK <sileht@sileht.net>
2022-08-22 15:22:09 +0200

    fix(redis): add connection error workaround (#745)
    Fixes MRGFY-1401

---
+++ mergify_engine/tests/unit/test_redis.py
@@ -0,0 +1,21 @@
+from mergify_engine import redis_utils
+
+
+async def test_redis_connection_error_working_behavior(
+    redis_links: redis_utils.RedisLinks,
+) -> None:
+    conn = await redis_links.cache.connection_pool.get_connection("whatever")
+    assert (
+        f"Error connecting to {conn.host}:{conn.port}. foobar."
+        == conn._error_message(ConnectionResetError("foobar"))
+    )
+
+
+async def test_redis_connection_error_workaround_for_broken_behavior(
+    redis_links: redis_utils.RedisLinks,
+) -> None:
+    conn = await redis_links.cache.connection_pool.get_connection("whatever")
+    assert (
+        f"Error connecting to {conn.host}:{conn.port}. Connection reset by peer."
+        == conn._error_message(ConnectionResetError())
+    )
