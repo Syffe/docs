97fe32892b6d656625f32d24403de44429498459
Mehdi ABAAKOUK <sileht@sileht.net>
2022-09-12 10:49:17 +0200

    refactor(refresh): add executor and type config (#1274)
    Fixes MRGFY-1444

--- mergify_engine/actions/refresh.py
+++ mergify_engine/actions/refresh.py
@@ -8,33 +8,45 @@ from mergify_engine import rules
 from mergify_engine import signals
 
 
-class RefreshCommand(actions.BackwardCompatAction):
-    flags = actions.ActionFlag.ALLOW_ON_CONFIGURATION_CHANGED
+class RefreshExecutorConfig(typing.TypedDict):
+    pass
 
-    validator: typing.ClassVar[typing.Dict[typing.Any, typing.Any]] = {}
 
-    async def run(
-        self, ctxt: context.Context, rule: rules.EvaluatedRule
-    ) -> check_api.Result:
+class RefreshExecutor(actions.ActionExecutor["RefreshCommand", RefreshExecutorConfig]):
+    @classmethod
+    async def create(
+        cls,
+        action: "RefreshCommand",
+        ctxt: "context.Context",
+        rule: "rules.EvaluatedRule",
+    ) -> "RefreshExecutor":
+        return cls(ctxt, rule, RefreshExecutorConfig())
+
+    async def run(self) -> check_api.Result:
         await refresher.send_pull_refresh(
-            ctxt.redis.stream,
-            ctxt.pull["base"]["repo"],
-            pull_request_number=ctxt.pull["number"],
+            self.ctxt.redis.stream,
+            self.ctxt.pull["base"]["repo"],
+            pull_request_number=self.ctxt.pull["number"],
             action="user",
             source="action/command",
         )
         await signals.send(
-            ctxt.repository,
-            ctxt.pull["number"],
+            self.ctxt.repository,
+            self.ctxt.pull["number"],
             "action.refresh",
             signals.EventNoMetadata(),
-            rule.get_signal_trigger(),
+            self.rule.get_signal_trigger(),
         )
         return check_api.Result(
             check_api.Conclusion.SUCCESS, title="Pull request refreshed", summary=""
         )
 
-    async def cancel(
-        self, ctxt: context.Context, rule: "rules.EvaluatedRule"
-    ) -> check_api.Result:  # pragma: no cover
+    async def cancel(self) -> check_api.Result:
         return actions.CANCELLED_CHECK_REPORT
+
+
+class RefreshCommand(actions.Action):
+    flags = actions.ActionFlag.ALLOW_ON_CONFIGURATION_CHANGED
+
+    validator: typing.ClassVar[typing.Dict[typing.Any, typing.Any]] = {}
+    executor_class = RefreshExecutor
