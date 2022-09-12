db1a2ae53dcb17e32c045a213cb7d7de62a4d488
Mehdi ABAAKOUK <sileht@sileht.net>
2022-09-12 17:20:06 +0200

    refactor(edit): add executor and type config (#1275)
    Fixes MRGFY-1437

--- mergify_engine/actions/edit.py
+++ mergify_engine/actions/edit.py
@@ -1,3 +1,5 @@
+import typing
+
 import voluptuous
 
 from mergify_engine import actions
@@ -12,7 +14,125 @@ from mergify_engine.dashboard import user_tokens
 from mergify_engine.rules import types
 
 
-class EditAction(actions.BackwardCompatAction):
+class EditExecutorConfig(typing.TypedDict):
+    draft: bool | None
+    bot_account: typing.Optional[user_tokens.UserTokensUser]
+
+
+class EditExecutor(actions.ActionExecutor["EditAction", EditExecutorConfig]):
+    @classmethod
+    async def create(
+        cls,
+        action: "EditAction",
+        ctxt: "context.Context",
+        rule: "rules.EvaluatedRule",
+    ) -> "EditExecutor":
+        try:
+            bot_account = await action_utils.render_bot_account(
+                ctxt,
+                action.config["bot_account"],
+                required_feature=subscription.Features.BOT_ACCOUNT,
+                missing_feature_message="Edit with `bot_account` set is disabled",
+                required_permissions=[],
+            )
+        except action_utils.RenderBotAccountFailure as e:
+            raise rules.InvalidPullRequestRule(e.title, e.reason)
+
+        github_user: typing.Optional[user_tokens.UserTokensUser] = None
+        tokens = await ctxt.repository.installation.get_user_tokens()
+
+        if bot_account:
+            github_user = tokens.get_token_for(bot_account)
+            if not github_user:
+                raise rules.InvalidPullRequestRule(
+                    f"Unable to edit: user `{bot_account}` is unknown. ",
+                    f"Please make sure `{bot_account}` has logged in Mergify dashboard.",
+                )
+        else:
+            github_user = tokens.users[0]
+
+        return cls(
+            ctxt,
+            rule,
+            EditExecutorConfig(
+                {"draft": action.config["draft"], "bot_account": github_user}
+            ),
+        )
+
+    async def run(self) -> check_api.Result:
+        if self.config["draft"] is None:
+            return check_api.Result(check_api.Conclusion.SUCCESS, "Nothing to do.", "")
+
+        if self.config["draft"]:
+            expected_state = True
+            current_state = "draft"
+            mutation = "convertPullRequestToDraft"
+        else:
+            expected_state = False
+            current_state = "ready for review"
+            mutation = "markPullRequestReadyForReview"
+
+        if self.ctxt.pull["draft"] == expected_state:
+            return check_api.Result(
+                check_api.Conclusion.SUCCESS,
+                f"Pull request is already {current_state}.",
+                "",
+            )
+
+        mutation = f"""
+            mutation {{
+                {mutation}(input:{{pullRequestId: "{self.ctxt.pull['node_id']}"}}) {{
+                    pullRequest {{
+                        isDraft
+                    }}
+                }}
+            }}
+        """
+        try:
+            await self.ctxt.client.graphql_post(
+                mutation,
+                oauth_token=self.config["bot_account"]["oauth_access_token"]
+                if self.config["bot_account"]
+                else None,
+            )
+        except github.GraphqlError as e:
+            if "Field 'convertPullRequestToDraft' doesn't exist" in e.message:
+                return check_api.Result(
+                    check_api.Conclusion.FAILURE,
+                    "Converting pull request to draft requires GHES >= 3.2",
+                    "",
+                )
+            self.ctxt.log.error(
+                "GraphQL API call failed, unable to convert PR.",
+                current_state=current_state,
+                response=e.message,
+            )
+            return check_api.Result(
+                check_api.Conclusion.FAILURE,
+                f"GraphQL API call failed, pull request wasn't converted to {current_state}.",
+                "",
+            )
+        self.ctxt.pull["draft"] = expected_state
+
+        await signals.send(
+            self.ctxt.repository,
+            self.ctxt.pull["number"],
+            "action.edit",
+            signals.EventEditMetadata({"draft": expected_state}),
+            self.rule.get_signal_trigger(),
+        )
+
+        return check_api.Result(
+            check_api.Conclusion.SUCCESS,
+            f"Pull request successfully converted to {current_state}",
+            "",
+        )
+
+    async def cancel(self) -> check_api.Result:
+        return actions.CANCELLED_CHECK_REPORT
+
+
+class EditAction(actions.Action):
     flags = (
         actions.ActionFlag.ALWAYS_RUN
         | actions.ActionFlag.ALLOW_ON_CONFIGURATION_CHANGED
@@ -23,106 +143,4 @@ class EditAction(actions.BackwardCompatAction):
         voluptuous.Required("draft", default=None): voluptuous.Any(None, bool),
     }
 
-    async def run(
-        self, ctxt: context.Context, rule: rules.EvaluatedRule
-    ) -> check_api.Result:
-        if self.config["draft"] is not None:
-            return await self._edit_draft_state(ctxt, rule, self.config["draft"])
-        return check_api.Result(
-            check_api.Conclusion.SUCCESS,
-            "Nothing to do.",
-            "",
-        )
-
-    async def cancel(
-        self, ctxt: context.Context, rule: "rules.EvaluatedRule"
-    ) -> check_api.Result:  # pragma: no cover
-        return actions.CANCELLED_CHECK_REPORT
-
-    async def _edit_draft_state(
-        self,
-        ctxt: context.Context,
-        rule: rules.EvaluatedRule,
-        draft_converted: bool,
-    ) -> check_api.Result:
-
-        if draft_converted:
-            expected_state = True
-            current_state = "draft"
-            mutation = "convertPullRequestToDraft"
-        else:
-            expected_state = False
-            current_state = "ready for review"
-            mutation = "markPullRequestReadyForReview"
-
-        if ctxt.pull["draft"] == expected_state:
-            return check_api.Result(
-                check_api.Conclusion.SUCCESS,
-                f"Pull request is already {current_state}.",
-                "",
-            )
-
-        try:
-            bot_account = await action_utils.render_bot_account(
-                ctxt,
-                self.config["bot_account"],
-                required_feature=subscription.Features.BOT_ACCOUNT,
-                missing_feature_message=f"{current_state} with `bot_account` set is disabled",
-                required_permissions=[],
-            )
-        except action_utils.RenderBotAccountFailure as e:
-            return check_api.Result(e.status, e.title, e.reason)
-
-        try:
-            tokens = await user_tokens.UserTokens.select_users_for(ctxt, bot_account)
-        except user_tokens.UserTokensUserNotFound as e:
-            return check_api.Result(
-                check_api.Conclusion.FAILURE, "Fail to convert pull request", e.reason
-            )
-
-        mutation = f"""
-            mutation {{
-                {mutation}(input:{{pullRequestId: "{ctxt.pull['node_id']}"}}) {{
-                    pullRequest {{
-                        isDraft
-                    }}
-                }}
-            }}
-        """
-        try:
-            await ctxt.client.graphql_post(
-                mutation,
-                oauth_token=tokens[0]["oauth_access_token"],
-            )
-        except github.GraphqlError as e:
-            if "Field 'convertPullRequestToDraft' doesn't exist" in e.message:
-                return check_api.Result(
-                    check_api.Conclusion.FAILURE,
-                    "Converting pull request to draft requires GHES >= 3.2",
-                    "",
-                )
-            ctxt.log.error(
-                "GraphQL API call failed, unable to convert PR.",
-                current_state=current_state,
-                response=e.message,
-            )
-            return check_api.Result(
-                check_api.Conclusion.FAILURE,
-                f"GraphQL API call failed, pull request wasn't converted to {current_state}.",
-                "",
-            )
-        ctxt.pull["draft"] = expected_state
-
-        await signals.send(
-            ctxt.repository,
-            ctxt.pull["number"],
-            "action.edit",
-            signals.EventEditMetadata({"draft": expected_state}),
-            rule.get_signal_trigger(),
-        )
-
-        return check_api.Result(
-            check_api.Conclusion.SUCCESS,
-            f"Pull request successfully converted to {current_state}",
-            "",
-        )
+    executor_class = EditExecutor
