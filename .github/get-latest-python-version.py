e0fc314474fcdf3ee9f9c23c6c096a2cdf1d3b62
Mehdi ABAAKOUK <sileht@sileht.net>
2022-10-14 09:37:02 +0200

    ci(python-update): improve new python detection (#2156)
    This change propose the pull request only if the new GitHub Action
    setup-python and the docker image has been released.

--- .github/get-latest-python-version.py
+++ .github/get-latest-python-version.py
@@ -6,17 +6,26 @@ import urllib.request as request
 
 
 def get_latest_version_number() -> str:
-    tags_request = request.urlopen(
+    github_action_request = request.urlopen(
+        "https://raw.githubusercontent.com/actions/python-versions/main/versions-manifest.json"
+    )
+    docker_request = request.urlopen(
         "https://hub.docker.com/v2/repositories/library/python/tags/?name=-slim&page_size=50"
     )
-    json_tags = json.load(tags_request)["results"]
-    json_tags = [
+
+    github_action_tags = json.load(github_action_request)
+    github_action_tags = {tag["version"] for tag in github_action_tags}
+
+    docker_tags = json.load(docker_request)["results"]
+    docker_tags = {
         tag["name"].replace("-slim", "")
-        for tag in json_tags
+        for tag in docker_tags
         if re.fullmatch(r"^\d+\.\d+\.\d+-slim", tag["name"])
-    ]
-    json_tags.sort(key=lambda s: list(map(int, s.split("."))))
-    return json_tags[-1]
+    }
+
+    common_tags = list(docker_tags & github_action_tags)
+    common_tags.sort(key=lambda s: list(map(int, s.split("."))))
+    return common_tags[-1]
 
 
 if __name__ == "__main__":
