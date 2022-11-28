ee2205b4420a523c05003eaa7749640d8d5a701b
Mehdi ABAAKOUK <sileht@sileht.net>
2022-11-28 09:01:48 +0100

    refactor(dismiss_reviews): use new action format (#2987)
    Fixes MRGFY-1448

--- mergify_engine/actions/dismiss_reviews.py
+++ mergify_engine/actions/dismiss_reviews.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 import typing
 
@@ -6,15 +8,23 @@ import voluptuous
 from mergify_engine import actions
 from mergify_engine import check_api
 from mergify_engine import context
+from mergify_engine import github_types
 from mergify_engine import rules
 from mergify_engine import signals
 from mergify_engine.clients import http
 from mergify_engine.rules import types
 
 
-WHEN_SYNCHRONIZE = "synchronize"
-WHEN_ALWAYS = "always"
-FROM_REQUESTED_REVIEWERS = "from_requested_reviewers"
+DismissReviewWhenT = typing.Literal["synchronize", "always"]
+WHEN_SYNCHRONIZE: DismissReviewWhenT = "synchronize"
+WHEN_ALWAYS: DismissReviewWhenT = "always"
+
+DismissReviewTypeT = typing.Union[
+    typing.Literal[True, False, "from_requested_reviewers"],  # noqa: NU003
+    list[github_types.GitHubLogin],
+]
+
+FROM_REQUESTED_REVIEWERS: DismissReviewTypeT = "from_requested_reviewers"
 
 DEFAULT_MESSAGE = {
     WHEN_SYNCHRONIZE: "Pull request has been modified.",
@@ -22,50 +32,55 @@ DEFAULT_MESSAGE = {
 }
 
 
-class DismissReviewsAction(actions.BackwardCompatAction):
-    flags = (
-        actions.ActionFlag.ALLOW_ON_CONFIGURATION_CHANGED
-        | actions.ActionFlag.ALWAYS_RUN
-    )
+class DismissReviewsExecutorConfig(typing.TypedDict):
+    approved: DismissReviewTypeT
+    changes_requested: DismissReviewTypeT
+    message: str | None
+    when: DismissReviewWhenT
 
-    validator = {
-        voluptuous.Required("approved", default=True): voluptuous.Any(
-            True,
-            False,
-            [types.GitHubLogin],
-            FROM_REQUESTED_REVIEWERS,
-        ),
-        voluptuous.Required("changes_requested", default=True): voluptuous.Any(
-            True,
-            False,
-            [types.GitHubLogin],
-            FROM_REQUESTED_REVIEWERS,
-        ),
-        voluptuous.Required("message", default=None): types.Jinja2WithNone,
-        voluptuous.Required("when", default=WHEN_SYNCHRONIZE): voluptuous.Any(
-            WHEN_SYNCHRONIZE, WHEN_ALWAYS
-        ),
-    }
 
-    async def run(
-        self, ctxt: context.Context, rule: rules.EvaluatedRule
-    ) -> check_api.Result:
-
-        if self.config["message"] is None:
-            message_raw = DEFAULT_MESSAGE[self.config["when"]]
+class DismissReviewsExecutor(
+    actions.ActionExecutor["DismissReviewsAction", DismissReviewsExecutorConfig]
+):
+    @classmethod
+    async def create(
+        cls,
+        action: DismissReviewsAction,
+        ctxt: context.Context,
+        rule: rules.EvaluatedRule,
+    ) -> DismissReviewsExecutor:
+        if action.config["message"] is None:
+            message_raw = DEFAULT_MESSAGE[action.config["when"]]
         else:
-            message_raw = typing.cast(str, self.config["message"])
+            message_raw = typing.cast(str, action.config["message"])
 
         try:
             message = await ctxt.pull_request.render_template(message_raw)
         except context.RenderTemplateFailure as rmf:
-            return check_api.Result(
-                check_api.Conclusion.FAILURE,
+            raise rules.InvalidPullRequestRule(
                 "Invalid dismiss reviews message",
                 str(rmf),
             )
 
-        if self.config["when"] == WHEN_SYNCHRONIZE and not ctxt.has_been_synchronized():
+        return cls(
+            ctxt,
+            rule,
+            DismissReviewsExecutorConfig(
+                {
+                    "message": message,
+                    "approved": action.config["approved"],
+                    "changes_requested": action.config["changes_requested"],
+                    "when": action.config["when"],
+                }
+            ),
+        )
+
+    async def run(self) -> check_api.Result:
+
+        if (
+            self.config["when"] == WHEN_SYNCHRONIZE
+            and not self.ctxt.has_been_synchronized()
+        ):
             return check_api.Result(
                 check_api.Conclusion.SUCCESS,
                 "Nothing to do, pull request has not been synchronized",
@@ -79,20 +94,20 @@ class DismissReviewsAction(actions.BackwardCompatAction):
         # This is only true for method="rebase"
         if (
             self.config["when"] == WHEN_SYNCHRONIZE
-            and not await ctxt.has_been_synchronized_by_user()
+            and not await self.ctxt.has_been_synchronized_by_user()
         ):
             return check_api.Result(
                 check_api.Conclusion.SUCCESS, "Updated by Mergify, ignoring", ""
             )
 
         requested_reviewers_login = {
-            rr["login"] for rr in ctxt.pull["requested_reviewers"]
+            rr["login"] for rr in self.ctxt.pull["requested_reviewers"]
         }
 
         to_dismiss = set()
         to_dismiss_users = set()
         to_dismiss_user_from_requested_reviewers = set()
-        for review in (await ctxt.consolidated_reviews())[1]:
+        for review in (await self.ctxt.consolidated_reviews())[1]:
             conf = self.config.get(review["state"].lower(), False)
             if conf is True:
                 to_dismiss.add(review["id"])
@@ -118,8 +133,8 @@ class DismissReviewsAction(actions.BackwardCompatAction):
             self.config.get("approved") == FROM_REQUESTED_REVIEWERS
             and to_dismiss_user_from_requested_reviewers
         ):
-            updated_pull = await ctxt.client.item(
-                f"{ctxt.base_url}/pulls/{ctxt.pull['number']}"
+            updated_pull = await self.ctxt.client.item(
+                f"{self.ctxt.base_url}/pulls/{self.ctxt.pull['number']}"
             )
             updated_requested_reviewers_login = {
                 rr["login"] for rr in updated_pull["requested_reviewers"]
@@ -129,7 +144,7 @@ class DismissReviewsAction(actions.BackwardCompatAction):
                 if updated_requested_reviewers_login != requested_reviewers_login
                 else logging.INFO
             )
-            ctxt.log.log(
+            self.ctxt.log.log(
                 level,
                 "about to dismiss approval reviews from requested_reviewers",
                 requested_reviewers_login=requested_reviewers_login,
@@ -140,9 +155,9 @@ class DismissReviewsAction(actions.BackwardCompatAction):
         errors = set()
         for review_id in to_dismiss:
             try:
-                await ctxt.client.put(
-                    f"{ctxt.base_url}/pulls/{ctxt.pull['number']}/reviews/{review_id}/dismissals",
-                    json={"message": message},
+                await self.ctxt.client.put(
+                    f"{self.ctxt.base_url}/pulls/{self.ctxt.pull['number']}/reviews/{review_id}/dismissals",
+                    json={"message": self.config["message"]},
                 )
             except http.HTTPClientSideError as e:  # pragma: no cover
                 errors.add(f"GitHub error: [{e.status_code}] `{e.message}`")
@@ -155,17 +170,43 @@ class DismissReviewsAction(actions.BackwardCompatAction):
             )
         else:
             await signals.send(
-                ctxt.repository,
-                ctxt.pull["number"],
+                self.ctxt.repository,
+                self.ctxt.pull["number"],
                 "action.dismiss_reviews",
                 signals.EventDismissReviewsMetadata({"users": list(to_dismiss_users)}),
-                rule.get_signal_trigger(),
+                self.rule.get_signal_trigger(),
             )
             return check_api.Result(
                 check_api.Conclusion.SUCCESS, "Review dismissed", ""
             )
 
-    async def cancel(
-        self, ctxt: context.Context, rule: "rules.EvaluatedRule"
-    ) -> check_api.Result:  # pragma: no cover
+    async def cancel(self) -> check_api.Result:  # pragma: no cover
         return actions.CANCELLED_CHECK_REPORT
+
+
+class DismissReviewsAction(actions.Action):
+    flags = (
+        actions.ActionFlag.ALLOW_ON_CONFIGURATION_CHANGED
+        | actions.ActionFlag.ALWAYS_RUN
+    )
+
+    validator = {
+        voluptuous.Required("approved", default=True): voluptuous.Any(
+            True,
+            False,
+            [types.GitHubLogin],
+            FROM_REQUESTED_REVIEWERS,
+        ),
+        voluptuous.Required("changes_requested", default=True): voluptuous.Any(
+            True,
+            False,
+            [types.GitHubLogin],
+            FROM_REQUESTED_REVIEWERS,
+        ),
+        voluptuous.Required("message", default=None): types.Jinja2WithNone,
+        voluptuous.Required("when", default=WHEN_SYNCHRONIZE): voluptuous.Any(
+            WHEN_SYNCHRONIZE, WHEN_ALWAYS
+        ),
+    }
+
+    executor_class = DismissReviewsExecutor
