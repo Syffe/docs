b9ac37dfcaae6a2a4a702000f7ba67d4ff8c8aa3
Mehdi ABAAKOUK <sileht@sileht.net>
2022-12-06 14:13:10 +0100

    chore: add tools to measures requests to GitHub in fixtures (#3167)

---
+++ tools/compare-github-requests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+count_requests() {
+    yq '.interactions[].request.uri | select(. | startswith("https://api.github.com"))' | wc -l
+}
+
+git_ref=${1:-HEAD^}
+
+files=$(git diff --name-only "$git_ref" 'zfixtures/*/http.yaml')
+
+[ -z "$files" ] && echo "No fixtures changed" && exit 1
+
+for file in $files; do
+    test=${file#zfixtures/cassette/}
+    test=${test%/http.yaml}
+    before=$(git cat-file blob $git_ref:$file | count_requests)
+    after=$(cat $file | count_requests)
+    echo "$test: $(($before-$after)) requests to github"
+done
