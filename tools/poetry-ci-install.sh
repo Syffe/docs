8599876b4612d77f9766949edb7c4867c14b8026
Mehdi ABAAKOUK <sileht@sileht.net>
2023-01-18 16:16:34 +0100

    ci(poetry): run workaround inside the poetry venv (#3900)

--- tools/poetry-ci-install.sh
+++ tools/poetry-ci-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 setuptools_version=$(grep -A1 'name = "setuptools"' poetry.lock | tail -n 1 | cut -d \" -f2)
-exec pip install "setuptools==${setuptools_version}"
+
+poetry run pip install "setuptools==${setuptools_version}"
+poetry install --sync
