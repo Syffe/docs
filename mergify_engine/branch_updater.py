981e079d39862beae4cc46e444b570cd02cfefa2
Mehdi ABAAKOUK <sileht@sileht.net>
2023-02-01 09:20:41 +0100

    chore(branch-updater): add more debug for rebase issue (#4129)

--- mergify_engine/branch_updater.py
+++ mergify_engine/branch_updater.py
@@ -141,9 +141,19 @@ async def _do_rebase(
         expected_sha = (await git("rev-parse", head_branch)).strip()
         if expected_sha:
             level = logging.INFO
+            extras = {}
         else:
             level = logging.ERROR
-        ctxt.log.log(level, "pull request rebased", new_head_sha=expected_sha)
+            extras = {
+                "logs": await git("log", "-100"),
+                "graph": await git(
+                    "log", "--graph", "--pretty=cshort", "--abbrev-commit", "-100"
+                ),
+                "status": await git("status"),
+                "branch": await git("branch", "-a"),
+                "remote": await git("remote", "-v"),
+            }
+        ctxt.log.log(level, "pull request rebased", new_head_sha=expected_sha, **extras)  # type: ignore[arg-type]
         # NOTE(sileht): We store this for Context.has_been_synchronized_by_user()
         await ctxt.redis.cache.setex(
             f"branch-update-{expected_sha}", 60 * 60, expected_sha
