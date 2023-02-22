5d33804d34f5541781c72d43f44767e1547d5c95
Mehdi ABAAKOUK <sileht@sileht.net>
2023-02-22 11:02:19 +0100

    chore: better test name (#4566)

--- tools/compare-github-requests.sh
+++ tools/compare-github-requests.sh
@@ -12,8 +12,9 @@ files=$(git diff --name-only $git_ref..HEAD 'zfixtures/*/http.yaml')
 [ -z "$files" ] && echo "No fixtures changed" && exit 0
 
 for file in $files; do
-    test=${file#zfixtures/cassette/}
+    test=${file#zfixtures/cassettes/}
     test=${test%/http.yaml}
+    test=${test//\//::}
     before=$(git cat-file blob $git_ref:$file | count_requests)
     after=$(cat $file | count_requests)
     echo "$test: $before -> $after ($(($after-$before))) requests to github"
