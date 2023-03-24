70dbebdbab2778f8ebc2273227eb241539efd52d
Syfe <97906012+Syffe@users.noreply.github.com>
2023-03-24 11:59:31 +0100

    ci(poetry): add verbosity to poetry ci (#5157)
    Add verbosity in order to debug flaky behavior

--- tools/poetry-ci-install.sh
+++ tools/poetry-ci-install.sh
@@ -2,5 +2,5 @@
 
 setuptools_version=$(grep -A1 'name = "setuptools"' poetry.lock | tail -n 1 | cut -d \" -f2)
 
-poetry run pip install "setuptools==${setuptools_version}"
-poetry install --sync
+poetry -vv run pip install "setuptools==${setuptools_version}"
+poetry -vv install --sync
