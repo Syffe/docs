9b15ee2effebbd972cafbcfd5ae18058b9db9f84
Mehdi ABAAKOUK <sileht@sileht.net>
2023-04-14 12:54:18 +0200

    docs: replace yarn by npm (#5563)
    Change-Id: Ibed7bab9d09bdd98f7fa1800661c5b63cdecb7dd

--- tools/build-docs.sh
+++ tools/build-docs.sh
@@ -15,11 +15,13 @@ rm -rf docs/build
 
 mergify-openapi-spec docs/build/api/openapi.json
 
-yarn --cwd docs
-yarn --cwd docs spectral lint -F hint build/api/openapi.json
-yarn --cwd docs sass source/scss/main.scss build/_bootstrap/bootstrap.css
-
-cp -f docs/node_modules/bootstrap/dist/js/bootstrap.min.js* docs/build/_bootstrap/
+(
+    cd docs
+    npm ci
+    npm exec spectral lint -F hint build/api/openapi.json
+    npm exec sass source/scss/main.scss build/_bootstrap/bootstrap.css
+    cp -f node_modules/bootstrap/dist/js/bootstrap.min.js* build/_bootstrap/
+)
 
 sphinx-build -W -b spelling docs/source docs/build
 sphinx-build -W -b dirhtml docs/source docs/build
