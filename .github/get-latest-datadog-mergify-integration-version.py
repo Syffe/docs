395ba72fc37dd6642fe954f9c84adfbf627ee2f1
Greesb <guillaumerisbourg@gmail.com>
2023-05-12 15:15:58 +0200

    ci(clifus): Add automatic updates for DD_MERGIFY_VERSION (#5985)
    Fixes MRGFY-2144

---
+++ .github/get-latest-datadog-mergify-integration-version.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import re
+import urllib.request as request
+
+
+VERSIONS_RE = re.compile(r"href=[\"']datadog_mergify-([\d\.]+)-py3-none-any.whl")
+
+
+def get_latest_version_number() -> str:
+    index_html_request = request.urlopen(
+        "https://dd-integrations-core-wheels-build-stable.datadoghq.com/targets/simple/datadog-mergify/index.html"
+    )
+    response = index_html_request.read().decode()
+    matchs = VERSIONS_RE.findall(response)
+    if not matchs:
+        raise Exception(  # noqa
+            "No versions found in request of datadog-mergify versions file"
+        )
+
+    matchs.sort(key=lambda s: list(map(int, s.split("."))))
+    return matchs[-1]
+
+
+if __name__ == "__main__":
+    print(get_latest_version_number())
