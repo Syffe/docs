b6246a0f4f7c14fb57f152c76d34bef52ff665cd
Mehdi ABAAKOUK <sileht@sileht.net>
2023-06-06 09:09:23 +0200

    chore: docker-compose left over (#6295)
    docker-compose has been replaced by docker compose, this is a leftover.

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -12,7 +12,7 @@ else
         ret="$?"
         set +exo pipefail
 
-        docker-compose down --remove-orphans --volumes
+        docker compose down --remove-orphans --volumes
         [ "$ret" == "0" -a "$MERGIFYENGINE_RECORD" == "1" ] && git add zfixtures/
 
         exit "$ret"
