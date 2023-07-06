6ff47f4788c122f1520879f84a467b122831d4c0
Damien Couppé <damien.couppe@mergify.com>
2023-07-06 15:15:13 +0200

    refactor: dump_schema to use the pg_dump of the pg docker to avoid version mismatch (#6657)
    Refacto dump_schema to use the pg_dump of the pg docker to avoid version mismatch

--- mergify_engine/tests/unit/test_database.py
+++ mergify_engine/tests/unit/test_database.py
@@ -1,9 +1,7 @@
 import asyncio
-import dataclasses
 import filecmp
 import os
 import pathlib
-import re
 import subprocess
 
 import pytest
@@ -12,24 +10,6 @@ from mergify_engine import settings
 from mergify_engine.config import types
 from mergify_engine.models import manage
 from mergify_engine.tests import utils
-
-
-@dataclasses.dataclass
-class DatabaseServerVersionMismatchError(Exception):
-    dump_error_message: dataclasses.InitVar[str]
-    server_version: str = dataclasses.field(init=False)
-    local_version: str = dataclasses.field(init=False)
-
-    def __post_init__(self, error_message: str) -> None:
-        self.server_version = re.search(  # type: ignore [union-attr]
-            r"server version: (\d+.\d+)", error_message
-        ).group(1)
-        self.local_version = re.search(  # type: ignore [union-attr]
-            r"pg_dump version: (\d+.\d+)", error_message
-        ).group(1)
-
-    def __str__(self) -> str:
-        return f"pg_dump is not up to date (server version={self.server_version}, local_version={self.local_version})"
 
 
 def _run_alembic(*args: str, environ: dict[str, str] | None = None) -> str:
@@ -78,7 +58,7 @@ def test_migration(setup_database: None, tmp_path: pathlib.Path) -> None:
     # fixture)
     loop = asyncio.get_event_loop_policy().new_event_loop()
     schema_dump_creation_path = tmp_path / "test_migration_create.sql"
-    dump_schema(settings.DATABASE_URL, schema_dump_creation_path)
+    dump_schema(settings.DATABASE_URL.path[1:], schema_dump_creation_path)
 
     if os.getenv("MIGRATED_DATA_DUMP") is None:
         url_migrate, url_migrate_without_db_name = utils.create_database_url(
@@ -91,7 +71,7 @@ def test_migration(setup_database: None, tmp_path: pathlib.Path) -> None:
         )
         _run_migration_scripts(url_migrate)
         schema_dump_migration_path = tmp_path / "test_migration_migrate.sql"
-        dump_schema(url_migrate, schema_dump_migration_path)
+        dump_schema("test_migration_migrate", schema_dump_migration_path)
     else:
         schema_dump_migration_path = pathlib.Path(os.environ["MIGRATED_DATA_DUMP"])
 
@@ -116,35 +96,29 @@ def test_migration(setup_database: None, tmp_path: pathlib.Path) -> None:
     loop.close()
 
 
-def dump_schema(url: types.PostgresDSN, filepath: pathlib.Path) -> None:
-    db_url = url.geturl().replace("postgresql+psycopg", "postgresql")
+def dump_schema(dbname: str, filepath: pathlib.Path) -> None:
+    pg_dump_cmd = [
+        "pg_dump",
+        "--no-acl",
+        "--no-owner",
+        "--no-comments",
+        f"--dbname={dbname}",
+        "--user=postgres",
+        "--exclude-schema=heroku_ext",
+        "--schema-only",
+        "--exclude-table=alembic_version",
+        "--format=p",
+        "--encoding=UTF8",
+    ]
+    process = subprocess.run(
+        ["docker", "exec", "postgres", *pg_dump_cmd],
+        check=True,
+        capture_output=True,
+        timeout=10,
+    )
 
-    try:
-        subprocess.run(
-            (
-                "pg_dump",
-                "--no-acl",
-                "--no-owner",
-                "--no-comments",
-                f"--dbname={db_url}",
-                "--exclude-schema=heroku_ext",
-                "--schema-only",
-                "--exclude-table=alembic_version",
-                "--format=p",
-                "--encoding=UTF8",
-                "--file",
-                filepath,
-            ),
-            check=True,
-            capture_output=True,
-            timeout=10,
-        )
-    except subprocess.CalledProcessError as e:
-        error_message = e.stderr.decode()
-        if "aborting because of server version mismatch" in error_message:
-            raise DatabaseServerVersionMismatchError(error_message)
-
-        raise
+    with open(filepath, "w") as f:
+        f.write(process.stdout.decode())
 
 
 def filediff(path1: pathlib.Path, path2: pathlib.Path) -> str | None:
