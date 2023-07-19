c36dd429601cc3f4e2c5a1ea8929340819abff38
Mehdi ABAAKOUK <sileht@sileht.net>
2023-07-19 17:06:40 +0200

    ci: automatically update production database (#6914)
    This change automatically updates production database
    
    Fixes MRGFY-2433

---
+++ tools/pg_url_to_env.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from urllib import parse
+
+
+def main() -> None:
+    # NOTE(sileht): we pass the url via env to ensure it can't leak in CI logs
+    url = os.environ[sys.argv[1]]
+    result = parse.urlparse(url)
+    assert result.username is not None
+    assert result.password is not None
+    print(
+        f"""
+export PGHOST="{result.hostname}"
+export PGPORT="{result.port}"
+export PGDATABASE="{result.path[1:]}"
+export PGUSER="{parse.unquote(result.username)}"
+export PGPASSWORD="{parse.unquote(result.password)}"
+    """
+    )
+
+
+if __name__ == "__main__":
+    main()
