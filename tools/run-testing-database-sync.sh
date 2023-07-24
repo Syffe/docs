a6d2d8c1ad23334fc25c235c71e4b0bce1a81441
Mehdi ABAAKOUK <sileht@sileht.net>
2023-07-24 12:27:00 +0200

    ci: re-add database migration testing (#7001)
    Here, the new approach.
    
    We have a new job that drop/restore the production database
    (engine_prod) into a new database (engine_ci_testing) within our unique
    RDS instance.
      * We use the binary format (the copy take only 2 minutes locally)
      * We don't anonymize the database as it's too slow and don't really
        matter as we don't copy the data on GitHub servers
      * We do the dump only once a day to not lock the production database
        on every deployment
    
    The deployment job now:
    * use the engine_ci_testing database to testing the upgrade with data
      that have less than one day
    * if it works we rollout we release heroku containers and upgrade the
      real database.
    
    Fixes MRGFY-2432

---
+++ tools/run-testing-database-sync.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# NOTE(sileht): ensure we don't print any environment variables
+# like PRODUCTION_DATABASE_BASE_URL that contains password
+set +xv
+
+if [ -z "$PRODUCTION_DATABASE_BASE_URL" ]; then
+    echo "PRODUCTION_DATABASE_BASE_URL is not set"
+    exit 1
+fi
+
+echo "* Creating the new database..."
+psql "$PRODUCTION_DATABASE_BASE_URL/postgres" -c "DROP DATABASE IF EXISTS engine_ci_testing WITH (FORCE);"
+psql "$PRODUCTION_DATABASE_BASE_URL/postgres" -c "CREATE DATABASE engine_ci_testing;"
+
+echo "* Wait for the new database to be ready..."
+while ! psql "$PRODUCTION_DATABASE_BASE_URL/engine_ci_testing" -c "select 1 as connected" ; do sleep 1 ; done
+
+echo "* Production database size"
+psql "$PRODUCTION_DATABASE_BASE_URL/postgres" -c "select pg_size_pretty(pg_database_size('engine_prod'));"
+
+echo "* Production database number of rows"
+psql "$PRODUCTION_DATABASE_BASE_URL/engine_prod" -c "SELECT schemaname,relname,n_live_tup FROM pg_stat_user_tables ORDER BY n_live_tup DESC;"
+
+echo "Syncing databases..."
+pg_dump --no-acl --no-owner --no-comments --format=c --encoding=UTF8 "$PRODUCTION_DATABASE_BASE_URL/engine_prod" \
+ | pg_restore --no-owner --no-acl --no-comments --exit-on-error -d "$PRODUCTION_DATABASE_BASE_URL/engine_ci_testing"
+
+echo "* CI Testing database size"
+psql "$PRODUCTION_DATABASE_BASE_URL/postgres" -c "select pg_size_pretty(pg_database_size('engine_ci_testing'));"
+
+echo "* CI Testing database number of rows"
+psql "$PRODUCTION_DATABASE_BASE_URL/engine_ci_testing" -c "SELECT schemaname,relname,n_live_tup FROM pg_stat_user_tables ORDER BY n_live_tup DESC;"
+
