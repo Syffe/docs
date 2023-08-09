83b4776e6b1b3827d064678eb0b21a649156b5a1
Mehdi ABAAKOUK <sileht@sileht.net>
2023-08-09 15:01:51 +0200

    fix(poetry): poetry has removed setuptools (#7250)
    We don't need this hack anymore
    
    Change-Id: Ic44140db77999fee6eec044f56fe44abfeb1128b

--- tools/poetry-ci-install.sh
+++ tools/poetry-ci-install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-setuptools_version=$(grep -A1 'name = "setuptools"' poetry.lock | tail -n 1 | cut -d \" -f2)
-
 if [ "$CI" ]; then
-    poetry run pip install --no-cache-dir "setuptools==${setuptools_version}"
     poetry install --sync --no-cache
 else
-    poetry run pip install "setuptools==${setuptools_version}"
     poetry install --sync
 fi
