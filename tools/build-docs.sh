2743d0620a9d19cc1e0bc6624d7c50f3c1349bca
Mehdi ABAAKOUK <sileht@sileht.net>
2023-09-25 10:51:47 +0200

    chore: drop old documentation (#7965)
    Change-Id: Id9bc5bceac0b7bf477a8a4bfc937cf8ed10f57d6

--- tools/build-docs.sh
+++ tools/build-docs.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
 set -eo pipefail
-
-git ls-files docs/*.rst | while read file; do 
-    if ! grep -q :description: $file; then 
-        echo E: no description meta tag in $file;
-        exit 1
-    fi
-done
-
 set -x
 
-rm -rf docs/build
-
-mergify-devtools generate-openapi-spec --visibility public docs/build/api/openapi.json
-mergify-devtools generate-openapi-spec --visibility public_future docs/build/api/openapi-future.json
-mergify-devtools generate-openapi-spec --visibility internal docs/build/api/openapi-internal.json
+mergify-devtools generate-openapi-spec --visibility public docs/openapi.json
+mergify-devtools generate-openapi-spec --visibility public_future docs/openapi-future.json
+mergify-devtools generate-openapi-spec --visibility internal docs/openapi-internal.json
 
 (
     cd docs
     npm ci
-    npm exec spectral lint -F hint build/api/openapi-future.json
-    npm exec sass source/scss/main.scss build/_bootstrap/bootstrap.css
-    cp -f node_modules/bootstrap/dist/js/bootstrap.min.js* build/_bootstrap/
+    npm exec spectral lint -F hint openapi-future.json
 )
-
-sphinx-build -W -b spelling docs/source docs/build
-sphinx-build -W -b dirhtml docs/source docs/build
