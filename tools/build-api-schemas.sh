50d33dc738a5be79a5b110d26220774a5bcade1d
Mehdi ABAAKOUK <sileht@sileht.net>
2023-09-28 10:43:08 +0200

    ci: rename docs stuffs into api-schemas (#8044)
    Change-Id: I862041c51fd41ac5622bb2fb522d55cfff1f07d8

---
+++ tools/build-api-schemas.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eo pipefail
+set -x
+
+mkdir -p api-schemas
+mergify-devtools generate-openapi-spec --visibility public api-schemas/public.json
+mergify-devtools generate-openapi-spec --visibility public_future api-schemas/public-future.json
+mergify-devtools generate-openapi-spec --visibility internal api-schemas/internal.json
+
+if [ "$CI" == "true" ]; then
+    FORMAT=github-actions
+else
+    FORMAT="stylish"
+fi
+
+npx -y --package @stoplight/spectral-cli -c "spectral lint -F hint -f $FORMAT api-schemas/public.json"
+npx -y --package @stoplight/spectral-cli -c "spectral lint -F hint -f $FORMAT api-schemas/public-futue.json"
