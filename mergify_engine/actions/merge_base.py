e161407a7c67d5c29d5e32765b844be2153034df
Mehdi ABAAKOUK <sileht@sileht.net>
2023-10-02 19:14:36 +0200

    fix(queue): merge retry must not be high (#8106)
    Using high for refreshing the PR mean we don't process other PR until those are
    merged. But these PR may not be mergeable ever. Change the priority to allow other PR to be merged.
    
    Fixes MRGFY-2701
    
    Change-Id: Ief7d06a9a3ba28e8f98d0369fc6ec7b931443577

--- mergify_engine/actions/merge_base.py
+++ mergify_engine/actions/merge_base.py
@@ -57,7 +57,7 @@ class MergeUtilsMixin:
                 pull_request_number=ctxt.pull["number"],
                 action="internal",
                 source="merge failed and need to be retried",
-                priority=worker_pusher.Priority.high,
+                priority=worker_pusher.Priority.medium,
                 refresh_flag=refresher.RefreshFlag.MERGE_FAILED,
                 max_attempts=cls.MAX_REFRESH_ATTEMPTS,
             )
