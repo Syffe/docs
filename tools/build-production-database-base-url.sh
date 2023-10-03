5159e55f790f6476a7942b03c7b49cd02cf96e4f
Mehdi ABAAKOUK <sileht@sileht.net>
2023-10-03 09:05:47 +0200

    ci: replace AWS secret by OIDC password-less AWS role (#8098)
    Fixes MRGFY-2694
    
    Change-Id: I25d77b08d13346d1fdd45eefc2b04e2f28ff51f4

---
+++ tools/build-production-database-base-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# NOTE(sileht): don't use this script in pull_request_target GitHub Workflow as it could leak the password
+
+function urlencode() {
+   python3 -c "from urllib.parse import quote, sys; print(quote(sys.stdin.read().encode().strip()))"
+}
+
+PRODUCTION_DATABASE_PASSWORD="$(aws rds generate-db-auth-token --hostname $PRODUCTION_DATABASE_HOST --port $PRODUCTION_DATABASE_PORT --region us-east-1 --username $PRODUCTION_DATABASE_USERNAME | urlencode)"
+echo "postgres://${PRODUCTION_DATABASE_USERNAME}:${PRODUCTION_DATABASE_PASSWORD}@${PRODUCTION_DATABASE_HOST}:${PRODUCTION_DATABASE_PORT}"
