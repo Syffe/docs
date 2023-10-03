7a10f67a2f332aaeb1b47b9bc6ca051e8d1d347a
Mehdi ABAAKOUK <sileht@sileht.net>
2023-10-03 11:52:57 +0200

    ci: replace non-breaking space by regular space (#8139)
    Change-Id: I046ab0cb6b7062570c93a93920977486c95611db

--- tools/build-production-database-base-url.sh
+++ tools/build-production-database-base-url.sh
@@ -5,5 +5,5 @@ function urlencode() {
    python3 -c "from urllib.parse import quote, sys; print(quote(sys.stdin.read().encode().strip()))"
 }
 
-PRODUCTION_DATABASE_PASSWORD="$(aws rds generate-db-auth-token --hostname $PRODUCTION_DATABASE_HOST --port $PRODUCTION_DATABASE_PORT --region us-east-1 --username $PRODUCTION_DATABASE_USERNAME | urlencode)"
+PRODUCTION_DATABASE_PASSWORD="$(aws rds generate-db-auth-token --hostname $PRODUCTION_DATABASE_HOST --port $PRODUCTION_DATABASE_PORT --region us-east-1 --username $PRODUCTION_DATABASE_USERNAME | urlencode)"
 echo "postgres://${PRODUCTION_DATABASE_USERNAME}:${PRODUCTION_DATABASE_PASSWORD}@${PRODUCTION_DATABASE_HOST}:${PRODUCTION_DATABASE_PORT}"
