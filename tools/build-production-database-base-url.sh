c9d9a9c773161c3683a486bd389ff58d65e69848
Mehdi ABAAKOUK <sileht@sileht.net>
2023-10-23 11:03:19 +0200

    ci(db-copy): urlencode all characters (#8483)
    pgsql need all characters encoded in password to parse the url correctly
    
    Change-Id: I8b5b1168afb2fb5b540442fc44e7a61719f43718

--- tools/build-production-database-base-url.sh
+++ tools/build-production-database-base-url.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 # NOTE(sileht): don't use this script in pull_request_target GitHub Workflow as it could leak the password
 
 function urlencode() {
-   python3 -c "from urllib.parse import quote, sys; print(quote(sys.stdin.read().encode().strip()))"
+   python3 -c "from urllib.parse import quote, sys; print(quote(sys.stdin.read().encode().strip(), safe=''))"
 }
 
 PRODUCTION_DATABASE_PASSWORD="$(aws rds generate-db-auth-token --hostname $PRODUCTION_DATABASE_HOST --port $PRODUCTION_DATABASE_PORT --region us-east-1 --username $PRODUCTION_DATABASE_USERNAME | urlencode)"
