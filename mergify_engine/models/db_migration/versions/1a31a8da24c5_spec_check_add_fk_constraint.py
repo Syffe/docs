7f0c65ac5c94d53c35af14c306d2abd69cf11439
Léo Ecrepont <57994579+lecrepont01@users.noreply.github.com>
2023-11-27 10:31:01 +0100

    refactor(events): remove old fk and create new constraint (#8941)
    We face the issue of not being able to leverage ON DELETE CASCADE for this associated table because the FK is on the parent (event) which is deleted. The FK need to be relocated to the "speculative_check_pull_request" table to do so.
    
    Third and last step we rebuild the new FK.
    
    Related to [MRGFY-2789](https://linear.app/mergify/issue/MRGFY-2789)
    Depends-On: https://github.com/Mergifyio/engine/pull/8786

---
+++ mergify_engine/models/db_migration/versions/1a31a8da24c5_spec_check_add_fk_constraint.py
@@ -0,0 +1,63 @@
+"""Spec check add fk constraint
+
+Revision ID: 1a31a8da24c5
+Revises: 8e8dfbc9c805
+Create Date: 2023-11-21 14:48:44.828049
+
+"""
+import alembic
+import sqlalchemy
+
+
+revision = "1a31a8da24c5"
+down_revision = "8e8dfbc9c805"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Drop old constraints
+    alembic.op.drop_constraint(
+        "event_action_queue_checks_end_speculative_check_pull_re_bcae",
+        "event_action_queue_checks_end",
+        type_="foreignkey",
+    )
+    alembic.op.drop_constraint(
+        "event_action_queue_checks_start_speculative_check_pull__7f8e",
+        "event_action_queue_checks_start",
+        type_="foreignkey",
+    )
+
+    # Create new constraint on prexisting, set non nullable
+    alembic.op.alter_column(
+        "speculative_check_pull_request",
+        "event_id",
+        existing_type=sqlalchemy.INTEGER(),
+        nullable=False,
+    )
+    alembic.op.create_foreign_key(
+        alembic.op.f("speculative_check_pull_request_event_id_fkey"),
+        "speculative_check_pull_request",
+        "event",
+        ["event_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
+    # Drop old FK constraints
+    alembic.op.drop_column(
+        "event_action_queue_checks_end",
+        "speculative_check_pull_request_id",
+    )
+    alembic.op.drop_column(
+        "event_action_queue_checks_start",
+        "speculative_check_pull_request_id",
+    )
+
+
+def downgrade() -> None:
+    # NOTE(sileht): We don't want to support downgrades as it means we will
+    # drop columns. And we don't want to provide tooling that may drop data.
+    # For restoring old version of the database, the only supported process is
+    # to use a backup.
+    pass
