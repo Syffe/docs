658f170929956625718ff16934ce96b29ab071f4
Greesb <guillaumerisbourg@gmail.com>
2023-12-07 15:05:42 +0100

    chore: add script to convert msgpack fixture to yaml (#9276)
    This will help to debug weird bugs happening between record and replay of a test.

---
+++ tools/fixtures-msgpack-to-yml.py
@@ -0,0 +1,22 @@
+import pathlib
+import sys
+
+import msgpack
+import yaml
+
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} test-name")
+    sys.exit(1)
+
+
+for file in pathlib.Path("zfixtures/cassettes").glob(f"**/{sys.argv[1]}/http.msgpack"):
+    print(f"Found file at {file}")
+    yaml_file_path = file.parent / "http.yaml"
+    with open(file, "rb") as msgpack_file:
+        data = msgpack.unpackb(msgpack_file.read())
+
+    with open(yaml_file_path, "w") as yaml_file:
+        yaml_file.write(yaml.dump(data))
+
+    print(f"Wrote yaml file at {yaml_file_path}")
