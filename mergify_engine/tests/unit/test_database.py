1ad682a3a4ec50d3bef41405db266fa25f3fd78e
Mehdi ABAAKOUK <sileht@sileht.net>
2023-12-12 09:52:12 +0100

    refactor(tests): avoid manipulating eventloop (#9371)
    We don't need to manage the event loop ourself, the problematic fixture
    has been removed.
    
    This change make tests async like other tests
    
    Change-Id: I5919636eba02a0e4a7282fbe37aa6ceaecb273b8

--- mergify_engine/tests/unit/test_database.py
+++ mergify_engine/tests/unit/test_database.py
@@ -24,7 +24,8 @@ from mergify_engine.models import manage
 from mergify_engine.tests import utils
 
 
-def _run_alembic(command: str, *args: str) -> str:
+def _alembic_thread(command: str, *args: str) -> str:
+    asyncio.set_event_loop(asyncio.new_event_loop())
     config = manage.load_alembic_config()
     config.stdout = io.StringIO()
 
@@ -38,23 +39,22 @@ def _run_alembic(command: str, *args: str) -> str:
     return config.stdout.getvalue()
 
 
-def _run_migration_scripts(url: config_types.PostgresDSN) -> None:
+async def _run_alembic(command: str, *args: str) -> str:
+    return await asyncio.to_thread(_alembic_thread, command, *args)
+
+
+async def _run_migration_scripts(url: config_types.PostgresDSN) -> None:
     with mock.patch.object(settings, "DATABASE_URL", url):
-        output = _run_alembic("history")
+        output = await _run_alembic("history")
         scripts_count = len(output.splitlines())
         for _ in range(scripts_count):
-            _run_alembic("upgrade", "+1")
+            await _run_alembic("upgrade", "+1")
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=sqlalchemy.exc.SAWarning)
-            _run_alembic("check")
+            await _run_alembic("check")
 
 
-def test_migration(_setup_database: None, tmp_path: pathlib.Path) -> None:
-    # We need to manually run the coroutine in an event loop because
-    # pytest-asyncio has its own `event_loop` fixture that is function scoped
-    # and in autouse (session scope fixture cannot require function scoped
-    # fixture)
-    loop = asyncio.get_event_loop_policy().new_event_loop()
+async def test_migration(_setup_database: None, tmp_path: pathlib.Path) -> None:
     schema_dump_creation_path = tmp_path / "test_migration_create.sql"
     utils.dump_schema(settings.DATABASE_URL.path[1:], schema_dump_creation_path)
 
@@ -62,13 +62,11 @@ def test_migration(_setup_database: None, tmp_path: pathlib.Path) -> None:
         url_migrate, url_migrate_without_db_name = utils.create_database_url(
             "test_migration_migrate",
         )
-        loop.run_until_complete(
-            utils.create_database(
-                url_migrate_without_db_name.geturl(),
-                "test_migration_migrate",
-            ),
+        await utils.create_database(
+            url_migrate_without_db_name.geturl(),
+            "test_migration_migrate",
         )
-        _run_migration_scripts(url_migrate)
+        await _run_migration_scripts(url_migrate)
         schema_dump_migration_path = tmp_path / "test_migration_migrate.sql"
         utils.dump_schema("test_migration_migrate", schema_dump_migration_path)
     else:
@@ -94,8 +92,6 @@ def test_migration(_setup_database: None, tmp_path: pathlib.Path) -> None:
         shallow=False,
     ), filediff(schema_dump_creation_path, schema_dump_migration_path)
 
-    loop.close()
-
 
 def filediff(path1: pathlib.Path, path2: pathlib.Path) -> str:
     with path1.open() as f1, path2.open() as f2:
@@ -108,8 +104,8 @@ def filediff(path1: pathlib.Path, path2: pathlib.Path) -> str:
         return "Database dump differences: \n" + "".join(diff)
 
 
-def test_one_head() -> None:
-    output = _run_alembic("heads")
+async def test_one_head() -> None:
+    output = await _run_alembic("heads")
     assert "(head)" in output
     heads = output.splitlines()
     assert (
