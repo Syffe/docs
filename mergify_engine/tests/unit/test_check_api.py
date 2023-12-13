d977a7e2bcb462e69264a04b96ab95cb5ba58fc1
Mehdi ABAAKOUK <sileht@sileht.net>
2023-12-13 16:11:12 +0100

    chore(tests): add coverage for check_api.compare_dict() (#9407)
    Change-Id: I5565333dfcd1a8cf681f2ecca8fe634aaac6a187

--- mergify_engine/tests/unit/test_check_api.py
+++ mergify_engine/tests/unit/test_check_api.py
@@ -1,6 +1,31 @@
+import typing
+
+import pytest
+
 from mergify_engine import check_api
 
 
 def test_conclusion_str() -> None:
     assert str(check_api.Conclusion(None)) == "🟠 pending"
     assert str(check_api.Conclusion("success")) == "✅ success"
+
+
+@pytest.mark.parametrize(
+    ("left", "right", "keys", "expected"),
+    (
+        ({}, {}, ("a",), True),
+        ({"a": 1}, {"a": 1}, ("a",), True),
+        ({"a": 1}, {"a": 2}, ("a",), False),
+        ({"a": 1}, {"a": 1}, ("a", "b"), True),
+        ({"a": 1, "b": 1}, {"a": 1, "b": 1}, ("a", "b"), True),
+        ({"a": 1, "b": 1}, {"a": 1, "b": 3}, ("a", "b"), False),
+        ({"a": 1}, {"a": 1}, (), True),
+    ),
+)
+def test_compare_dict(
+    left: dict[str, typing.Any],
+    right: dict[str, typing.Any],
+    keys: tuple[str, ...],
+    expected: bool,
+) -> None:
+    assert check_api.compare_dict(left, right, keys) == expected
