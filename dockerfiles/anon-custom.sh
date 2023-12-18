d7d0a91bbc160264c6b0861fc13452a8109ebd6e
Julien Danjou <julien@danjou.info>
2023-12-18 11:13:20 +0100

    ci: fail on any psql error (#9517)
    This makes sure any anonymization rules issue is raised in the CI.
    
    Related to #9516
    
    Change-Id: If036f6acc4e3fde70ffd6f6fe64060c3ca3f082b

--- dockerfiles/anon-custom.sh
+++ dockerfiles/anon-custom.sh
@@ -13,6 +13,6 @@ gosu postgres pg_ctl start
 gosu postgres psql -c "ALTER SYSTEM SET session_preload_libraries = 'anon';"
 gosu postgres psql -c "SELECT pg_reload_conf();"
 
-cat | gosu postgres psql
+gosu postgres psql -v ON_ERROR_STOP=1
 
 /usr/bin/pg_dump_anon
