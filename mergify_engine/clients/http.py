28fbb3a71e9a5aab5f7c16f26b18b441f4c25549
Greesb <guillaumerisbourg@gmail.com>
2023-12-27 10:33:45 +0100

    fix: do not pass `transport` to `httpx.AsyncClient` (#9683)
    It breaks prod but needs fixing, so we'll fix this in another PR. It wasn't passed in the first place.
    
    Partial revert of https://github.com/Mergifyio/engine/commit/9658f061358ae2bd43460e825308a0a2975bdaf0
    
    Fixes INC-116

--- mergify_engine/clients/http.py
+++ mergify_engine/clients/http.py
@@ -276,7 +276,9 @@ class AsyncClient(httpx.AsyncClient):
         headers: httpx_types.HeaderTypes | None = None,
         timeout: httpx_types.TimeoutTypes = DEFAULT_TIMEOUT,
         base_url: httpx_types.URLTypes = "",
-        transport: httpx.AsyncBaseTransport | None = None,
+        # FIXME: This should be passed in the __init__ but it breaks production
+        # INC-116
+        transport: httpx.AsyncBaseTransport | None = None,  # noqa: ARG002
         retry_stop_after_attempt: int | None = None,
         retry_exponential_multiplier: float | None = None,
     ) -> None:
@@ -300,7 +302,6 @@ class AsyncClient(httpx.AsyncClient):
             base_url=base_url,
             headers=final_headers,
             timeout=timeout,
-            transport=transport,
             follow_redirects=True,
         )
 
