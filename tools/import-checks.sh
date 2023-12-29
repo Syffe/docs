6a24d20376ea96ca90fabdbcfa05931a4c39be12
Greesb <guillaumerisbourg@gmail.com>
2023-12-29 12:23:41 +0100

    ci: add pytest to import-checks when testing with dev deps (#9704)
    Some import errors are not picked up with the checks we already have, but are picked up by pytest,
    which is ran in tools/check-obsolete-fixtures.sh which leads to this script to fail and "hide"
    the error by displaying all the folders as unused.
    We still need to be able to run the import-checks with only prod deps.

--- tools/import-checks.sh
+++ tools/import-checks.sh
@@ -7,3 +7,6 @@ set -ex
 mergify-database-update --help
 mergify-engine-worker --enabled-services ""
 python3 -m mergify_engine.web.asgi
+if [ -n "$IMPORT_CHECKS_TEST_DEV_DEPS" ]; then
+    pytest --collect-only -q
+fi
